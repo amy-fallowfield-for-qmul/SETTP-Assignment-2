0b0c7faac758a91d5b49b183c718e6288d907b56
Amy Fallowfield <ajfallow@amazon.com>
2026-03-27 10:19:11 +0000

    Add data storage class

---
+++ src/Data/dataStorage.py
@@ -0,0 +1,36 @@
+import csv
+from typing import List
+
+class DataStorage:
+    """Singleton handler for CSV file input/output"""
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self) -> None:
+        if not hasattr(self, '_initialised'):
+            self._initialised = True
+
+    def save_to_csv(self, path: str, headers: List[str], rows: List[List[str]]) -> None:
+        try:
+            with open(path, "w") as file:
+                writer = csv.writer(file)
+                writer.writerow(headers)
+
+                for row in rows:
+                    writer.writerow(row)
+        except Exception as e:
+            raise(e)
+
+    def load_from_csv(self, path: str) -> List[List[str]]:
+        try:
+            with open(path, "r") as file:
+                reader = csv.reader(file)
+                next(reader)
+                return [row for row in reader]
+        except Exception as e:
+            raise(e)
