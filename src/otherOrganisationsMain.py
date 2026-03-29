f204a60c8dc26e2ca26c2ccb18fad6a28db93893
Amy Fallowfield <ajfallow@amazon.com>
2026-03-29 17:57:09 +0100

    Create structure for other organisations

--- src/otherOrganisationsMain.py
+++ src/otherOrganisationsMain.py
@@ -1,7 +1,18 @@
+from abc import abstractmethod
+from typing import List
 from mainABC import MainABC
+from constants import SEPARATION_WIDTH
 
 class OtherOrganisationMain(MainABC):
-    """Singleton entry point for the Digital ID System used by other organisations"""
+    """Abstract base class for specific other organisations"""
+
+    @property
+    @abstractmethod
+    def allowed_attributes(self) -> List[str]: pass
+
+    @property
+    @abstractmethod
+    def organisation_name(self) -> str: pass
 
     def generate_options(self) -> None:
         print("\nPlease select an option:")
@@ -16,11 +27,40 @@ class OtherOrganisationMain(MainABC):
 
         match(choice):
             case 1:
-                self.REQUESTS.query_id()
+                self._query_permissions()
             case 2:
                 self.REQUESTS.exit_program()
             case _:
                 print("Invalid choice")
 
-if __name__ == "__main__":
-    program = OtherOrganisationMain()
+    def _query_permissions(self) -> None:
+        try:
+            id_subject = self.REQUESTS._get_id_subject()
+            attribute_choice = self._get_attribute_choice()
+            justification = input("Enter justification for query: ")
+            
+            current_value = self.REQUESTS.DIGITAL_ID_SERVICE.query_attribute(
+                id_subject.id, attribute_choice, justification, self.organisation_name, self.allowed_attributes
+            )
+
+            print("=" * SEPARATION_WIDTH)
+            print(f"ID: {id_subject.id}, {attribute_choice}: {current_value}")
+            print("=" * SEPARATION_WIDTH)
+        except ValueError as e:
+            print(f"Error querying allowed attribtues: {e}")
+
+    def _get_attribute_choice(self) -> str:
+        try:
+            print(f"Please select the attribute you wish to query:")
+            for i, field in enumerate(self.allowed_attributes, start=1):
+                print(f"{i}. {field}")
+
+            user_choice = int(input())
+            if user_choice < 1 or user_choice > len(self.allowed_attributes):
+                raise
+
+            attribute_choice = self.allowed_attributes[user_choice - 1]
+            return attribute_choice
+
+        except:
+            raise ValueError("Invalid input")
