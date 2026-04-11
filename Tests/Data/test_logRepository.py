82cc65a52019eea7565dfdee0e0d408326af1905
Amy Fallowfield <ajfallow@amazon.com>
2026-04-11 19:00:23 +0200

    Add tests for adding updated attribute to log objects

--- Tests/Data/test_logRepository.py
+++ Tests/Data/test_logRepository.py
@@ -13,7 +13,7 @@ class TestLogRepositoryAddAndGet:
     """Tests for adding and retrieving logs"""
 
     def test_add(self, log_repo: LogRepository) -> None:
-        log = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None)
+        log = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None, None)
         log_repo.add(log)
         assert len(log_repo.get_all()) == 1
         stored_log = log_repo.get_from_id(log.id)
@@ -30,8 +30,8 @@ class TestLogRepositoryAddAndGet:
         assert stored_log.new_value is None
 
     def test_add_multiple_logs(self, log_repo: LogRepository) -> None:
-        log1 = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None)
-        log2 = Log(False, "HMRC", 2, Action.READ, "Tax check", "active", None)
+        log1 = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None, None)
+        log2 = Log(False, "HMRC", 2, Action.READ, "Tax check", "active", None, None)
         log_repo.add(log1)
         log_repo.add(log2)
         assert len(log_repo.get_all()) == 2
@@ -40,7 +40,7 @@ class TestLogRepositoryAddAndGet:
         assert log_repo.get_all() == {}
 
     def test_get_from_id(self, log_repo: LogRepository) -> None:
-        log = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None)
+        log = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None, None)
         log_repo.add(log)
         retrieved_log = log_repo.get_from_id(log.id)
         assert retrieved_log == log
@@ -64,7 +64,7 @@ class TestLogRepositoryCSV:
 
     def test_save_to_csv(self, monkeypatch) -> None:
         monkeypatch.setattr(self.log_repo, "_get_csv_path", lambda: self.TEST_CSV_PATH)
-        log = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None)
+        log = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None, None)
         self.log_repo.add(log)
         self.log_repo.save_to_csv()
         assert os.path.exists(self.TEST_CSV_PATH)
@@ -72,20 +72,21 @@ class TestLogRepositoryCSV:
         with open(self.TEST_CSV_PATH, "r") as file:
             lines = file.readlines()
 
-        assert lines[0].strip() == "id,timestamp,accepted,organisation,digitalID,action,justification,currentValue,newValue"
+        assert lines[0].strip() == "id,timestamp,accepted,organisation,digitalID,action,justification,currentValue,newValue,attribute"
         data_line = lines[1].strip().split(",")
         start_time = datetime.now()
         assert data_line[0] == str(log.id)
         csv_timestamp = datetime.strptime(data_line[1], "%d/%m/%Y - %H:%M:%S")
         assert csv_timestamp >= start_time.replace(microsecond=0)
         assert csv_timestamp <= datetime.now()
-        assert data_line[2] == "True"  # accepted field
+        assert data_line[2] == "True"
         assert data_line[3] == "NHS"
         assert data_line[4] == "1"
         assert data_line[5] == "create"
         assert data_line[6] == "New registration"
         assert data_line[7] == "John Smith"
         assert data_line[8] == ""
+        assert data_line[9] == ""
 
     def test_save_empty_csv(self, monkeypatch) -> None:
         monkeypatch.setattr(self.log_repo, "_get_csv_path", lambda: self.TEST_CSV_PATH)
@@ -96,12 +97,12 @@ class TestLogRepositoryCSV:
             lines = file.readlines()
 
         assert len(lines) == 1
-        assert lines[0].strip() == "id,timestamp,accepted,organisation,digitalID,action,justification,currentValue,newValue"
+        assert lines[0].strip() == "id,timestamp,accepted,organisation,digitalID,action,justification,currentValue,newValue,attribute"
 
     def test_load_from_csv(self, monkeypatch) -> None:
         monkeypatch.setattr(self.log_repo, "_get_csv_path", lambda: self.TEST_CSV_PATH)
-        log1 = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None)
-        log2 = Log(False, "HMRC", 2, Action.UPDATE, "Name change", "John", "Alicia")
+        log1 = Log(True, "NHS", 1, Action.CREATE, "New registration", "John Smith", None, None)
+        log2 = Log(False, "HMRC", 2, Action.UPDATE, "Name change", "John", "Alicia", "first_name")
         self.log_repo.add(log1)
         self.log_repo.add(log2)
         self.log_repo.save_to_csv()
