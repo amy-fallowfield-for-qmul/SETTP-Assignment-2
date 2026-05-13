c50d760e0735f5b6174496233583e85f85a932e7
Amy Fallowfield <ajfallow@amazon.com>
2026-05-13 19:22:04 +0100

    Remove unnecessary exceptions

--- src/Data/dataStorage.py
+++ src/Data/dataStorage.py
@@ -2,23 +2,15 @@ import csv
 from typing import List
 
 def save_to_csv(path: str, headers: List[str], rows: List[List[str]]) -> None:
-    try:
-        with open(path, "w") as file:
-            writer = csv.writer(file)
-            writer.writerow(headers)
+    with open(path, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
 
-            for row in rows:
-                writer.writerow(row)
-    except Exception as e:
-        raise IOError(f"Failed to save CSV file at {path}: {e}")
+        for row in rows:
+            writer.writerow(row)
 
 def load_from_csv(path: str) -> List[List[str]]:
-    try:
-        with open(path, "r") as file:
-            reader = csv.reader(file)
-            next(reader)
-            return [row for row in reader]
-    except FileNotFoundError:
-        raise FileNotFoundError(f"CSV file not found at {path}")
-    except Exception as e:
-        raise IOError(f"Failed to save CSV file at {path}: {e}")
+    with open(path, "r") as file:
+        reader = csv.reader(file)
+        next(reader)
+        return [row for row in reader]
